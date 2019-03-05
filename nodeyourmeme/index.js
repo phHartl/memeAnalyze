d0379610cf10fc06fc2b08cf957aaaf8ba925069
scaredn00b <Philipp1.Hartl@stud.uni-regensburg.de>
2019-03-05 23:31:19 +0100

    Added delay between request to avoid ip bans. Additionally now all picture of a meme are getting crawled correctly (there has been a calculation error before) as well as google OCR is called for everything.

--- nodeyourmeme/index.js
+++ nodeyourmeme/index.js
@@ -7,30 +7,16 @@ module.exports = require('./api');
 
 client = new vision.ImageAnnotatorClient();
 
-// module.exports.search('Philosoraptor').then(function (result) {
-//   textRecognitionByGoogle(result.image).then(function (res) {
-//     console.log("Google text recognition of main image\n" +res[0].description);
-//   });
-//   fs.writeFile(result.name + ".txt", result.about, function (err) {
-//
-//   });
-//   download(result.image, result.name + '.jpg', function () {
-//     console.log("downloaded main picture");
-//   });
-//   for (let i = 0; i < result.examples_images.length; i++) {
-//     download(result.examples_images[i], result.name + "_example" + [i + 1] + ".jpg", function () {
-//       console.log("downloaded example")
-//     })
-//   }
-//   ;
-//   module.exports.searchPhotos(result.url).then(function (res) {
-//     for (let i = 0; i < res.recent_examples.length; i++) {
-//       download(res.recent_examples[i], result.name + '_recentExample' +[i]+ ".jpg", function () {
-//         console.log("downloaded recent example");
-//       })
-//     }
-//   });
-// }).catch(console.error);
+module.exports.search('Philosoraptor').then(function (result) {
+  (async () => {
+    let memes2D = await Promise.all(result);
+    //Convert the corresponding 2D array into a 1D to save as csv
+    let memes = [].concat(...memes2D);
+    let csv = new objectsToCsv(memes);
+    // Save to file:
+    await csv.toDisk('./memes.csv');
+  })();
+}).catch(console.error);
 //
 // module.exports.random().then(function (result) {
 //   textRecognitionByGoogle(result.image).then(function (res) {
@@ -57,29 +43,13 @@ var download = function (uri, filename, callback) {
   });
 };
 
-module.exports.topImageMacros(1).then(function (result) {
-  (async () => {
-    let memes2D = await Promise.all(result);
-    //Convert the corresponding 2D array into a 1D to save as csv
-    let memes = [].concat(...memes2D);
-    let csv = new objectsToCsv(memes);
-    // Save to file:
-    await csv.toDisk('./memes.csv');
-  })();
-});
-
-//Performs an API Request -> JSON File with credentials needed
-function textRecognitionByGoogle(fileName) {
-  return new Promise(function (resolve, reject) {
-    client
-      .textDetection(fileName)
-      .then(results => {
-        const detections = results[0].textAnnotations;
-        if (detections.length !== 0) {
-          resolve(detections);
-        } else {
-          reject("no text detected")
-        }
-      });
-  })
-};
+// module.exports.topImageMacros(1).then(function (result) {
+//   (async () => {
+//     let memes2D = await Promise.all(result);
+//     //Convert the corresponding 2D array into a 1D to save as csv
+//     let memes = [].concat(...memes2D);
+//     let csv = new objectsToCsv(memes);
+//     // Save to file:
+//     await csv.toDisk('./memes.csv');
+//   })();
+// });
